5625d527022e19973de043fa5c825224c3ea7dfa
ChunHao Kao <jerkkao@gmail.com>
2016-03-19 21:00:29 +0800

    change url rounting

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from views import home, collection #, datavisualv2, dvviewing, dvothers,dvall, dvhotword, datavisual, resume
+from views import home, collection #, pages #, datavisualv2, dvviewing, dvothers,dvall, dvhotword, datavisual, resume
 
 urlpatterns = [
     url(r'^sharoncollection/$',home,name='home'),
-    url(r'^sharoncollection/(?P<name>\w+)',collection,name='collection'),
-    '''url(r'^datavisualv2',datavisualv2,name='datavisualv2'),
-    url(r'^dvviewing',dvviewing,name='dvviewing'),
-    url(r'^dvothers',dvothers,name='dvothers'),
-    url(r'^dvall',dvall,name='dvall'),
-    url(r'^dvhotword',dvhotword,name='dvhotword'),
-    url(r'^datavisual',datavisual,name='datavisual'),
-    url(r'^resume',resume,name='resume'),'''   
+    url(r'^sharoncollection/(?P<name>\w+)$',collection,name='collection'),
+    #url(r'^sharoncollection/(?P<name>\w+)/(?P<pages>\w+)',pages,name='pages'),
     #url(r'^admin/', include(admin.site.urls)),
 ]
