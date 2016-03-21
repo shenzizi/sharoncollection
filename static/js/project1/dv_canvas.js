84fb80a7be21d6bda669ab04e16e2c9c8cea1456
ChunHao Kao <jerkkao@gmail.com>
2016-03-21 11:00:48 +0800

    use the more common file name

---
+++ static/js/project1/dv_canvas.js
@@ -0,0 +1,453 @@
+// set the width and height of canvas as its parent
+var c = $('#canvas');
+      var ct = c.get(0).getContext('2d');
+      var container = $(c).parent().parent();
+      //var container = $("#canvascontainer");
+      c.attr('width', $(container).width() )
+      c.attr('height', $(c).height() );
+
+function processdata_click(data){
+  function processdata(){
+   var dataset=[],dataname=[],radiuses=[]; 
+   for (var i=0;i<data.data.docs.length;i++){ 
+    var obj={};
+    obj.name=data.data.docs[i].v
+    obj.size=data.data.docs[i].occs
+    dataset.push(obj)
+  }
+     //retrieve the object key&values,save name as an array and size as another array
+     for (var i=0;i<dataset.length;i++){
+      var name=dataset[i].name,size=dataset[i].size
+      dataname.push(name)
+      radiuses.push(size)
+    }
+   // console.log(dataname,radiuses)
+    return [dataname,radiuses]
+  }
+      var name=processdata(data)[0],
+      size=processdata(data)[1]
+      window.onload = draw(name,size,data);
+      /*var c = $('#canvas');
+      var ct = c.get(0).getContext('2d');
+      var container = $(c).parent();
+      c.attr('width', $(container).width() );*/ //max width
+      // handle the response evnet
+      $(window).resize( function(){ 
+        c.attr('width', $(container).width() ); //max width
+        c.attr('height', $(container).height() );//max height
+        processdata_click(data) 
+    }); 
+};
+
+
+   $('#top10').click(function(){
+    processdata_click(top10_data)
+   })
+   $('#top15').click(function(){
+    processdata_click(top15_data)
+   })
+   $('#top20').click(function(){
+    processdata_click(top20_data)
+   })
+   $('#top28').click(function(){
+     processdata_click(top28_data)
+   })
+
+    //Get the canvas & context 
+    /*var c = $('#canvas');
+    var ct = c.get(0).getContext('2d');
+    var container = $(c).parent()
+    var container = $(c).parent().parent().parent();//var container=$(divid)
+    $(window).resize( function(){ 
+        c.attr('width', $(container).width() ); //max width
+        c.attr('height', $(container).height() );//max height
+        var dataname=c.attr('class')
+        processdata_click(top20_data) 
+      });*/
+    
+    //});//document.ready
+
+// ===========================
+// ancillary geometric classes
+// ===========================
+var Point = function (x, y)
+{
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype = {
+  dist: function (p) { return this.vect(p).norm(); },
+  vect: function (p) { return new Point (p.x-this.x, p.y-this.y); },
+  norm: function (p) { return Math.sqrt (this.x*this.x+this.y*this.y);},
+  add : function (v) { return new Point (this.x + v.x, this.y + v.y);},
+  mult: function (a) { return new Point (this.x * a, this.y * a);}
+};
+var Circle = function (radius, center)
+{
+  this.r = radius;
+  this.c = center;
+};
+
+Circle.prototype = {
+  surface:  function () { return Math.PI * this.r * this.r; },
+  distance: function (circle) { return this.c.dist(circle.c) - this.r - circle.r; }
+};
+
+
+// =========================
+// circle packer lives here!
+// =========================
+var Packer = function (circles, ratio)
+{
+  this.circles = circles;
+  this.ratio   = ratio || 1;
+  this.list = this.solve();
+}
+
+Packer.prototype = {
+    // try to fit all circles into a rectangle of a given surface
+    compute: function (surface)
+    {
+        // check if a circle is inside our rectangle
+        function in_rect (radius, center)
+        {
+          if (center.x - radius < - w/2) return false;
+          if (center.x + radius >   w/2) return false;
+          if (center.y - radius < - h/2) return false;
+          if (center.y + radius >   h/2) return false;
+          return true;
+        }
+
+        // approximate a segment with an "infinite" radius circle
+        function bounding_circle (x0, y0, x1, y1)
+        {
+          var xm = Math.abs ((x1-x0)*w);
+          var ym = Math.abs ((y1-y0)*h);
+          var m = xm > ym ? xm : ym;
+          var theta = Math.asin(m/4/bounding_r);
+          var r = bounding_r * Math.cos (theta);
+          return new Circle (bounding_r, 
+            new Point (r*(y0-y1)/2+(x0+x1)*w/4, 
+             r*(x1-x0)/2+(y0+y1)*h/4));
+        }
+        
+        // return the corner placements for two circles
+        function corner (radius, c1, c2)
+        {
+            var u = c1.c.vect(c2.c); // c1 to c2 vector
+            var A = u.norm();
+            if (A == 0) return [] // same centers
+            u = u.mult(1/A); // c1 to c2 unary vector
+            // compute c1 and c2 intersection coordinates in (u,v) base
+            var B = c1.r+radius;
+            var C = c2.r+radius;
+            if (A > (B + C)) return []; // too far apart
+            var x = (A + (B*B-C*C)/A)/2;
+            var y = Math.sqrt (B*B - x*x);
+            var base = c1.c.add (u.mult(x));
+            
+            var res = [];
+            var p1 = new Point (base.x -u.y * y, base.y + u.x * y);
+            var p2 = new Point (base.x +u.y * y, base.y - u.x * y);
+            if (in_rect(radius, p1)) res.push(new Circle (radius, p1));
+            if (in_rect(radius, p2)) res.push(new Circle (radius, p2));
+            return res;
+          }
+
+        /////////////////////////////////////////////////////////////////
+        
+        // deduce starting dimensions from surface
+        var bounding_r = Math.sqrt(surface) * 100; // "infinite" radius
+        var w = this.w = Math.sqrt (surface * this.ratio);
+        var h = this.h = this.w/this.ratio;
+        
+        // place our bounding circles
+        var placed=[
+        bounding_circle ( 1,  1,  1, -1),
+        bounding_circle ( 1, -1, -1, -1),
+        bounding_circle (-1, -1, -1,  1),
+        bounding_circle (-1,  1,  1,  1)];
+        
+        // Initialize our rectangles list
+        var unplaced = this.circles.slice(0); // clones the array
+        while (unplaced.length > 0)
+        {
+            // compute all possible placements of the unplaced circles
+            var lambda = {};
+            var circle = {};
+            for (var i = 0 ; i != unplaced.length ; i++)
+            {
+              var lambda_min = 1e10;
+              lambda[i] = -1e10;
+                // match current circle against all possible pairs of placed circles
+                for (var j = 0   ; j < placed.length ; j++)
+                  for (var k = j+1 ; k < placed.length ; k++)
+                  {
+                    // find corner placement
+                    if (k > 3) {
+                      zog=1;
+                    }
+                    var corners = corner (unplaced[i], placed[j], placed[k]);
+
+                    // check each placement
+                    for (var c = 0 ; c != corners.length ; c++)
+                    {
+                        // check for overlap and compute min distance
+                        var d_min = 1e10;
+                        for (var l = 0 ; l != placed.length ; l++)
+                        {
+                            // skip the two circles used for the placement
+                            if (l==j || l==k) continue;
+
+                            // compute distance from current circle
+                            var d = placed[l].distance (corners[c]);
+                            if (d < 0) break; // circles overlap
+                            
+                            if (d < d_min) d_min = d;
+                          }
+                        if (l == placed.length) // no overlap
+                        {
+                          if (d_min < lambda_min)
+                          {
+                            lambda_min = d_min;
+                            lambda[i] = 1- d_min/unplaced[i];
+                            circle[i] = corners[c];
+                          }
+                        }
+                      }
+                    }
+                  }
+
+            // select the circle with maximal gain
+            var lambda_max = -1e10;
+            var i_max = -1;
+            for (var i = 0 ; i != unplaced.length ; i++)
+            {
+              if (lambda[i] > lambda_max)
+              {
+                lambda_max = lambda[i];
+                i_max = i;
+              }
+            }
+            
+            // failure if no circle fits
+            if (i_max == -1) break;
+            
+            // place the selected circle
+            unplaced.splice(i_max,1);
+            placed.push (circle[i_max]);
+          }
+
+        // return all placed circles except the four bounding circles
+        this.tmp_bounds = placed.splice (0, 4);
+        return placed;
+      },
+
+    // find the smallest rectangle to fit all circles
+    solve: function ()
+    {
+        // compute total surface of the circles
+        var surface = 0;
+        for (var i = 0 ; i != this.circles.length ; i++)
+        {
+          surface += Math.PI * Math.pow(this.circles[i],2);
+        }
+        
+        // set a suitable precision
+        var limit = surface/1000;
+        var step = surface/2;
+        var res = [];
+        while (step > limit)
+        {
+          var placement = this.compute.call (this, surface);
+          //console.log ("placed",placement.length,"out of",this.circles.length,"for surface", surface);
+          if (placement.length != this.circles.length)
+          {
+            surface += step;
+          }
+          else
+          {
+            res = placement;
+            this.bounds = this.tmp_bounds;
+            surface -= step;
+          }
+          step /= 2;
+        }
+        return res; 
+      }
+    };
+
+// ====
+// demo
+// ====
+
+var fillcolor=["#1f77b4","#aec7e8"," #ff7f0e","#ffbb78","#2ca02c","#98df8a"," #d62728"," #ff9896","#9467bd","#c5b0d5","#8c564b","#c49c94","#e377c2","#f7b6d2","#7f7f7f", "#637939","#bcbd22","#dbdb8d","#17becf","#9edae5","#393b79","#c7c7c7","#8c6d31", "#ad494a"," #7b4173","#ce6dbd"," #cedb9c","#9c9ede","#e7ba52","#ad494a","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue"]
+
+function draw_result (packer,data)
+{   
+  var xyposition=[];
+  var topleftposition=[];
+  var circlesg=[];
+  function draw_circle(circle)
+  {   
+    var circlesgobj={}; 
+        ctx.font = "bold 15px Microsoft JhengHei";
+        //ctx.globalAlpha=0.5;
+        ctx.beginPath();
+        ctx.arc((circle.c.x+dx)*zoom+mx, (circle.c.y+dy)*zoom+my, circle.r*zoom, 0, 2*Math.PI); 
+        xyposition.push((circle.c.x+dx)*zoom+mx, (circle.c.y+dy)*zoom+my);      
+        ctx.fillStyle=fillcolor[i];  
+        ctx.fill();
+        //ctx.fillStyle="black";
+        circlesgobj.left=(circle.c.x+dx)*zoom+mx- circle.r*zoom;
+        circlesgobj.right=(circle.c.x+dx)*zoom+mx+ circle.r*zoom;
+        circlesgobj.top=(circle.c.y+dy)*zoom+my- circle.r*zoom;
+        circlesgobj.bottom=(circle.c.y+dy)*zoom+my+ circle.r*zoom;
+        circlesgobj.x=Math.floor((circle.c.x+dx)*zoom+mx);
+        circlesgobj.y=Math.floor((circle.c.y+dy)*zoom+my);
+        circlesgobj.r=Math.floor(circle.r*zoom)
+        topleftposition.push(circlesgobj.left+33,circlesgobj.top+20)
+        circlesg.push(circlesgobj)               
+      }
+
+      var canvas = document.getElementById ('canvas');
+      var ctx = canvas.getContext("2d");
+    canvas.width +=0; // clear canvas
+    var margin_factor = 0; //the magin b/w the canvas & rect
+    
+    var mx = canvas.width * margin_factor / 2;
+    var my = canvas.height* margin_factor / 2;
+    var dx = packer.w/2;
+    var dy = packer.h/2;
+    var zx = canvas.width  * (1-margin_factor) / packer.w;
+    var zy = canvas.height * (1-margin_factor) / packer.h;
+    var zoom = zx < zy ? zx : zy;
+    
+    
+    // draw all circles
+    for (var i = 0 ; i != packer.list.length ; i++){
+      draw_circle (packer.list[i]);
+    }
+    
+
+    // draw bounding circles
+    //ctx.strokeStyle = 'red';
+    //for (var i = 0 ; i != packer.bounds.length ; i++)
+        //draw_circle (packer.bounds[i]);/*cancel the border width
+        
+    // draw rectangle
+    /*ctx.strokeStyle = '#530890';
+    ctx.beginPath();
+    ctx.rect((-packer.w/2+dx)*zoom+mx, (-packer.h/2+dy)*zoom+my, packer.w*zoom, packer.h*zoom);
+    ctx.closePath();
+    ctx.stroke();*/
+
+
+   function mapping_r_name(data){
+     //console.log(data);
+     var circle_r=_.pluck(circlesg,"r"); 
+     var circle_x=_.pluck(circlesg,"x"); 
+     var circle_y=_.pluck(circlesg,"y");
+     var circle_r_index=circle_r.concat().sort()
+     circle_r_index.sort(function(x, y){return x-y})
+     var circle_indexarray=[];
+     for (i=0;i<circle_r.length;i++){
+       circle_indexarray.push(circle_r_index.indexOf(circle_r[i]))
+     }
+     var dataset_order=[]
+     for (var i=0;i<data.data.docs.length;i++){ 
+      var obj={};
+      obj.name=data.data.docs[i].v
+      obj.size=data.data.docs[i].occs
+      dataset_order.push(obj)
+    }
+    var name=_.pluck(dataset_order,"name");
+    var size=_.pluck(dataset_order,"size");
+    var dataset_r_index=size.concat().sort()
+    dataset_r_index.sort(function(x, y){return x-y})
+    var dataset_indexarray=[]; 
+    for (i=0;i<size.length;i++){
+     dataset_indexarray.push(dataset_r_index.indexOf(size[i]))
+    }
+    for (var i=0;i<dataset_order.length;i++){
+      dataset_order[i].size=dataset_indexarray[i]
+    }  
+    //console.log(circle_indexarray,dataset_order)
+    var items=dataset_order; 
+    var sorting = circle_indexarray; 
+    var name_result = []
+    sorting.forEach(function(key) {
+      var found = false;
+      items = items.filter(function(item) {
+        if(!found && item.size == key) {
+            name_result.push(item);
+            found = true;
+            return false;
+        } else 
+            return true;
+     })
+    })
+    var name_order=_.pluck(name_result,"name")
+    //console.log(name_order,dataset_order)
+    return name_order
+  }; 
+   
+   var name_order=mapping_r_name(data);
+
+  //filltext, line break
+    for (var i=0;i<name_order.length;i++){
+      var datanamei = name_order[i].split(' ')
+      for (var j = 0; j<datanamei.length; j++){
+        if (circlesg[i].r<80 && datanamei.length>2){
+          ctx.fillStyle = "floralwhite";
+          ctx.fillText(datanamei[j],topleftposition[i*2],topleftposition[i*2+1]+(j*20))
+        }else{
+          ctx.fillStyle = "floralwhite";
+          ctx.fillText(datanamei[j],xyposition[i*2],xyposition[i*2+1]+(j*20))
+        }
+      }
+    }
+    //console.log(circlesg) 
+    //console.log(xyposition)
+
+   /*$('#canvas').click(function (e) {
+    var clickedX = e.pageX - this.offsetLeft;
+    var clickedY = e.pageY - this.offsetTop;
+    console.log(clickedX,clickedY)
+      //console.log(circlesg)
+      for (var i = 0; i < circlesg.length; i++) {
+        if (clickedX < circlesg[i].right && clickedX > circlesg[i].left && clickedY > circlesg[i].top && clickedY < circlesg[i].bottom) {
+         alert(circlesg[i].name)
+       }
+     }
+   });*/
+ }
+
+ function draw (name,size,data){
+  //console.log(name,size)
+  dataname=name;
+  radiuses= size;
+  data=data; 
+  //console.log(radiuses)
+  var circles=radiuses.length;
+  var min_r=Math.min(...radiuses);
+  var max_r=Math.max(...radiuses); 
+  var Rscale = d3.scale.linear()
+  .domain([min_r, max_r])
+  .range([10, 50]); 
+  radiuses=radiuses.map(function(size){
+    return Rscale(size)
+  })
+  var c = $('#canvas');
+  var ct = c.get(0).getContext('2d');
+  var container = $(c).parent()
+  var rwdwidth=$(container).width();
+  var rwdheight=$(container).height();
+  //console.log(container,rwdwidth,rwdheight)
+  ratio=rwdwidth/rwdheight; 
+  //console.log(ratio)
+  var packer = new Packer (radiuses, ratio);
+  draw_result(packer,data);  
+  }
+  processdata_click(top20_data)
---
+++ static/js/project1/dv_data.js
@@ -0,0 +1,111 @@
+var hardcode_viewingbubble_json_oneweek = {"viewing": {"新台灣加油": 0.6, "正晶限時批": 0.36, "新聞面對面": 0.49, "新聞龍捲風": 0.58, "少康戰情室": 0.4, "新聞深喉嚨": 0.55}}, 
+    hardcode_viewingbubble_jsonvalue_oneweek=[0.6,0.36,0.49,0.58,0.4,0.55]
+var hardcode_viewingbubble_json_onemonth = {"viewing": {"新台灣加油": 0.63, "正晶限時批": 0.6, "新聞面對面": 0.39, "新聞龍捲風": 0.4, "少康戰情室": 0.25, "新聞深喉嚨": 0.32}}, 
+    hardcode_viewingbubble_jsonvalue_onemonth=[0.63,0.6,0.39,0.4,0.25,0.32]
+var hardcode_viewingbubble_json_oneyear = {"viewing": {"新台灣加油": 0.62, "正晶限時批": 0.47, "新聞面對面": 0.32, "新聞龍捲風": 0.5, "少康戰情室": 0.29, "新聞深喉嚨": 0.37}}, 
+    hardcode_viewingbubble_jsonvalue_oneyear=[0.62,0.47,0.32,0.5,0.29,0.37]
+
+var hardcode_pie_barline_data_oneweek=[{name:"新台灣加油",response:100,adcost:25000,cpm:450},{name:"正晶限時批",response:20,adcost:35000,cpm:400},{name:"新聞面對面",response:50,adcost:20000,cpm:450},{name:"新聞龍捲風",response:6,adcost:15000,cpm:250},{name:"少康戰情室",response:10,adcost:15000,cpm:550},{name:"新聞深喉嚨",response:10,adcost:37000,cpm:370}]; 
+var hardcode_pie_barline_data_onemonth=[{name:"新台灣加油",response:150,adcost:28000,cpm:475},{name:"正晶限時批",response:40,adcost:32000,cpm:380},{name:"新聞面對面",response:50,adcost:20000,cpm:420},{name:"新聞龍捲風",response:8,adcost:20000,cpm:280},{name:"少康戰情室",response:12,adcost:15000,cpm:650},{name:"新聞深喉嚨",response:20,adcost:33000,cpm:350}]; 
+var hardcode_pie_barline_data_oneyear=[{name:"新台灣加油",response:180,adcost:26000,cpm:400},{name:"正晶限時批",response:75,adcost:31000,cpm:400},{name:"新聞面對面",response:55,adcost:22000,cpm:400},{name:"新聞龍捲風",response:16,adcost:22000,cpm:230},{name:"少康戰情室",response:14,adcost:25000,cpm:670},{name:"新聞深喉嚨",response:35,adcost:28000,cpm:320}]; 
+
+
+var hardcode_bardata_oneweek=[{name:"新台灣加油",pnvalue:50},{name:"正晶限時批",pnvalue:20},{name:"新聞面對面",pnvalue:15},{name:"新聞龍捲風",pnvalue:5},{name:"少康戰情室",pnvalue:2},{name:"新聞深喉嚨",pnvalue:3},{name:"新台灣加油",pnvalue:-20},{name:"正晶限時批",pnvalue:-2},{name:"新聞面對面",pnvalue:-6},{name:"新聞龍捲風",pnvalue:-5},{name:"少康戰情室",pnvalue:-4},{name:"新聞深喉嚨",pnvalue:0}]; 
+var hardcode_bardata_onemonth=[{name:"新台灣加油",pnvalue:80},{name:"正晶限時批",pnvalue:50},{name:"新聞面對面",pnvalue:25},{name:"新聞龍捲風",pnvalue:25},{name:"少康戰情室",pnvalue:6},{name:"新聞深喉嚨",pnvalue:4},{name:"新台灣加油",pnvalue:-40},{name:"正晶限時批",pnvalue:-10},{name:"新聞面對面",pnvalue:-6},{name:"新聞龍捲風",pnvalue:-10},{name:"少康戰情室",pnvalue:-8},{name:"新聞深喉嚨",pnvalue:-2}]; 
+var hardcode_bardata_oneyear=[{name:"新台灣加油",pnvalue:120},{name:"正晶限時批",pnvalue:40},{name:"新聞面對面",pnvalue:35},{name:"新聞龍捲風",pnvalue:55},{name:"少康戰情室",pnvalue:9},{name:"新聞深喉嚨",pnvalue:9},{name:"新台灣加油",pnvalue:-48},{name:"正晶限時批",pnvalue:-42},{name:"新聞面對面",pnvalue:-12},{name:"新聞龍捲風",pnvalue:-25},{name:"少康戰情室",pnvalue:-14},{name:"新聞深喉嚨",pnvalue:-10}]; 
+
+var hardcode_layout_data = [
+    { "len": 4, "color": "#8dd3c7", "label": "新聞面對面", "id": "新聞面對面"},
+    { "len": 4, "color": "#ffffb3", "label": "正晶限時批", "id": "正晶限時批" },
+    { "len": 4, "color": "#bebada", "label": "新聞龍捲風", "id": "新聞龍捲風" },
+    { "len": 4, "color": "#fb8072", "label": "新聞深喉嚨", "id": "新聞深喉嚨" },
+    { "len": 4, "color": "#b3de69", "label": "少康戰情室", "id": "少康戰情室" },
+    { "len": 4, "color": "#fdb462", "label": "新台灣加油", "id": "新台灣加油" },
+];
+
+//viewing
+var hardcode_viewing = [
+    ['新聞面對面',0,1,0.64],
+    ['新聞面對面',1,2,0.64],
+    ['新聞面對面',2,3,0.64],
+    ['新聞面對面',3,4,0.49],
+    ['正晶限時批',0,1,0.51],
+    ['正晶限時批',1,2,0.52],
+    ['正晶限時批',2,3,0.49],
+    ['正晶限時批',3,4,0.33],
+    ['新聞龍捲風',0,1,0.47],
+    ['新聞龍捲風',1,2,0.49],
+    ['新聞龍捲風',2,3,0.43],
+    ['新聞龍捲風',3,4,0.55],
+    ['新聞深喉嚨',0,1,0.46],
+    ['新聞深喉嚨',1,2,0.48],
+    ['新聞深喉嚨',2,3,0.47],
+    ['新聞深喉嚨',3,4,0.6],
+    ['少康戰情室',0,1,0.29],
+    ['少康戰情室',1,2,0.27],
+    ['少康戰情室',2,3,0.29],
+    ['少康戰情室',3,4,0.45],
+    ['新台灣加油',0,1,0.85],
+    ['新台灣加油',1,2,0.79],
+    ['新台灣加油',2,3,0.85],
+    ['新台灣加油',3,4,0.57],
+];
+//c.p.m
+var hardcode_cpm = [
+    ['新聞面對面',0,1,194],
+    ['新聞面對面',1,2,194],
+    ['新聞面對面',2,3,194],
+    ['新聞面對面',3,4,253],
+    ['正晶限時批',0,1,195],
+    ['正晶限時批',1,2,191],
+    ['正晶限時批',2,3,203],
+    ['正晶限時批',3,4,301],
+    ['新聞龍捲風',0,1,370],
+    ['新聞龍捲風',1,2,355],
+    ['新聞龍捲風',2,3,404],
+    ['新聞龍捲風',3,4,316],
+    ['新聞深喉嚨',0,1,378],
+    ['新聞深喉嚨',1,2,362],
+    ['新聞深喉嚨',2,3,370],
+    ['新聞深喉嚨',3,4,290],
+    ['少康戰情室',0,1,428],
+    ['少康戰情室',1,2,460],
+    ['少康戰情室',2,3,428],
+    ['少康戰情室',3,4,276],
+    ['新台灣加油',0,1,146],
+    ['新台灣加油',1,2,157],
+    ['新台灣加油',2,3,146],
+    ['新台灣加油',3,4,218],
+];
+
+
+function tooltip(){ 
+  var tooltipvalue=[]; 
+    for (i=0;i<hardcode_viewing.length;i++){ 
+     var tooltipobj={};    
+      tooltipobj.name=hardcode_viewing[i][0];
+      tooltipobj.value=hardcode_viewing[i][3];
+        if (i%4==0){
+          tooltipobj.date="12/22"
+        }else if(i%4==1){
+          tooltipobj.date="12/23"
+        }else if(i%4==2){
+          tooltipobj.date="12/24"
+        }else{
+          tooltipobj.date="12/25"
+        }
+      tooltipvalue.push(tooltipobj);
+    }    
+    return tooltipvalue
+}
+
+function barlinechart(){
+    var chartvalue_viewing=[];
+    for (i=0;i<hardcode_viewing.length;i++){ 
+        viewingvalue=hardcode_viewing[i][3];
+        cpmvalue=hardcode_cpm[i][3];        
+        name=hardcode_viewing[i][0];
+        chartvalue_viewing.push(viewingvalue,cpmvalue,name);
+    }
+    return chartvalue_viewing
+}
